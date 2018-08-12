9e3b1213b1b74e8b3e38a2ba9ab5ae8216fe55c0
nkiang <nkiang@gmail.com>
2018-08-12 21:14:13 +0800

    sth

---
+++ ios1/CardModule/CardDialog.h
@@ -0,0 +1,28 @@
+//
+//  CardDialog.h
+//  CardModule
+//
+//  Created by admin on 2018/8/2.
+//  Copyright © 2018年 Erdem Başeğmez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+
+
+typedef void(^backBolock)(NSDictionary * );
+
+@interface CardDialog : UIView<UIPickerViewDataSource,UIPickerViewDelegate>
+
+@property (strong,nonatomic)UIPickerView *pick;
+
+@property(nonatomic,copy)backBolock bolock;
+
+//创建一个数组来传递返回的值
+@property(nonatomic,strong)NSString *backString;
+
+-(instancetype)initWithFrame:(CGRect)frame;
+
+@end
